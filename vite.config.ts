963afef73c5db7d8002e82c4ab7cf2497099695e
peccu <peccul+github@gmail.com>
2025-09-24 22:49:35 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: process.env.NODE_ENV === 'production' ? '/qr-code-reader/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
